cff4422c07060290eebc02e3c9ffdccf9344d324
michael williams <michael.williams@washpost.com>
2018-03-27 01:05:45 -0400

    Updated schema file

--- RESTlite_Users_db.sql
+++ RESTlite_Users_db.sql
@@ -36,7 +36,7 @@ CREATE TABLE `comments` (
   KEY `fk_comments_user_idx` (`user_id`),
   CONSTRAINT `fk_comments_message_id` FOREIGN KEY (`message_id`) REFERENCES `messages` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_comments_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,6 +45,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,1,2,'I have read that book and agree it is good but still not my favorite compared to The Invisible Man by Ralph Ellison.','2018-03-03 23:48:37',NULL),(2,3,1,'The latest message show up at the top so this will need to be fixed later.','2018-03-26 23:53:51',NULL);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +65,7 @@ CREATE TABLE `messages` (
   PRIMARY KEY (`id`),
   KEY `fk_message_user_idx` (`user_id`),
   CONSTRAINT `fk_message_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +74,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,1,'My favorite book is Form Whom the Bell Tolls by Ernest Hemingway.','2018-03-03 23:47:39',NULL),(2,6,'This is just a test message for everyone to see.','2018-03-04 16:18:55',NULL),(3,1,'This is the refactored and updated wall.','2018-03-26 23:35:24',NULL);
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,8 +97,19 @@ CREATE TABLE `users` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Andrew','Williams','mawfia@yahoo.com','6101c312c0b1714711558b47cbc89d11','99487600d4e0dec9b22b490cfca6cd','1981-03-06 00:00:00',3,'2018-03-03 00:42:40','2018-03-27 00:34:27'),(2,'Abraham','Lincoln','a.lincoln@whitehouse.gov','9f1e807c23c612170f32cb1bc8b5cf9b','71cb46ee11e09d050218e0341c035e','1976-02-06 00:00:00',2,'2018-03-03 17:56:54','2018-03-26 21:36:43'),(4,'Donald','Trump','dtrump@whitehouse.gov','6ea97158f2977b61fdab62dffe9d7398','f3cfab506b0f95db0b81c699335ecf','1972-01-01 00:00:00',1,'2018-03-04 04:36:21','2018-03-27 00:16:51'),(6,'Daren','Danvey','Danzy@DanzyLove.com','32669612c754ee716451894d1a294d70','51e8e21b42aa46b846634157d8e5c1','1977-01-21 00:00:00',3,'2018-03-04 16:17:13','2018-03-26 23:56:56');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -106,4 +119,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-03  0:40:01
+-- Dump completed on 2018-03-27  1:05:27
